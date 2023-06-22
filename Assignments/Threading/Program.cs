2e0d16e11f1d10253afa8417a1370b2859e51855
AbhijeetHabe <abhijeethabe23@gmail.com>
2023-06-22 22:57:24 +0530

    Threading concepts

---
+++ Assignments/Threading/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "main thread";
+            Console.WriteLine(mainThread.Name);
+
+            Thread thread1 = new Thread(() => CountDown("timer #1"));
+            Thread thread2 = new Thread(() => CountUp("timer #2"));
+            thread1.Start();
+            thread2.Start();
+
+            //CountDown();
+            //CountUp();
+        }
+
+        public static void CountDown(string name)
+        {
+            for (int i = 10; i>=0; i--)
+            {
+                Console.WriteLine($"{name}: "+i+" seconds");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine($"{name} is complete");
+        }
+
+        public static void CountUp(string name)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{name}: " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine($"{name} is complete");
+        }
+    }
+}
