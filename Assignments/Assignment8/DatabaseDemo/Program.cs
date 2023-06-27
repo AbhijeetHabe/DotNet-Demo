e761fc6a3de2ff9a59210936631f6a95fa33cf2c
AbhijeetHabe <abhijeethabe23@gmail.com>
2023-06-27 19:01:34 +0530

    Database connection demo

---
+++ Assignments/Assignment8/DatabaseDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DatabaseDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection cn = new SqlConnection();
+            //cn.ConnectionString = "Data Source=NameOfSqlServer;Initial Catalog=DatabaseName;User Id=sa;Password=pwd";
+            //cn.ConnectionString = "Data Source=NameOfSqlServer;Initial Catalog=DatabaseName;Integrated Security=true";
+            //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=True;Connect Timeout=30;
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            Console.WriteLine("open");
+
+            cn.Close();
+        }
+    }
+}
